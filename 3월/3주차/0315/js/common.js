b9040d8a99d45c07cad412ec93ed39647bd729ae
songsu218 <asane7@naver.com>
2024-03-17 15:17:15 +0900

    study

--- 3월/3주차/0315/js/common.js
+++ 3월/3주차/0315/js/common.js
@@ -1,3 +1,16 @@
+//.ham 클릭하면
+//.gnb & .person이 나타나게 해주세욤..
+//.gnb & .person 에 .on 클래스 붙여주세요,
+//let
+const $ham = document.querySelector(".ham");
+const $gnb = document.querySelector(".gnb");
+const $person = document.querySelector(".person");
+$ham.addEventListener("click", (e) => {
+  // console.log(e);
+  $gnb.classList.toggle("on");
+  $person.classList.toggle("on");
+});
+
 let mainSlide = new Swiper(".mainSlide", {
   spaceBetween: 30,
   effect: "fade",
@@ -12,4 +25,21 @@ let mainSlide = new Swiper(".mainSlide", {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
   },
+});
+
+var swiper = new Swiper(".eduSlide", {
+  slidesPerView: 5,
+  spaceBetween: 150,
+  pagination: {
+    el: ".pg2",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  // 가운데정렬 기능
+  centeredSlides: true,
+  // 무한반복기능
+  loop: true,
 });
