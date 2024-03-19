80dce07492a9b60dd966b2b18b3f4fb41e3c0175
songsu218 <asane7@naver.com>
2024-03-19 09:25:50 +0900

    js study 1

---
+++ 3월/4주차/0318/j02.js
@@ -0,0 +1,2 @@
+// 숫자, 문자, 불리언, null, undefinde, 객체
+// number
---
+++ 3월/4주차/0318/test.js
@@ -0,0 +1,6 @@
+// .sample 선택해서
+// "안녕"얼 입력해주시오.
+
+let text = document.querySelector(".sample");
+console.log(text);
+text.innerHTML = "안녕";
